c5376b7ca00e79a0842dce1b8e399ef0538649d1
john odoyo <161558213+Jaykoyi1@users.noreply.github.com>
2024-02-28 00:48:42 -0800

    Update use_name.py

--- use_name.py
+++ use_name.py
@@ -1,2 +1,9 @@
-name = Testing
-print (len(name))
+#Use the input() function to ask the user for their name and store it in a variable called "name".
+name = input("Enter User name: ")
+#Use the input() function to ask the user for their age and store it in a variable called "age"
+age = input("Enter user age: ")
+#Use the input() function to ask the user for their location and store it in a variable called "location".
+location = input("Enter Location: ")
+
+#Print out a personalized message u
+print(f"Hello{name}, you are {age} years old and you live in {location}")
